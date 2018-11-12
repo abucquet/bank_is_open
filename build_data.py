79a1fb573389449b0ef164a581bfe300eaa10f03
Alexandre Bucquet <bucqueta@stanford.edu>
2018-11-11 16:56:27 -0800

    first data cleaning

---
+++ build_data.py
@@ -0,0 +1,70 @@
+#### Reading the json file data
+
+
+# What we need to know about a given game between two teams:
+### Per game info:
+
+
+import json
+import os
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+
+directory = "bank_is_open/basketball_reference-master/matches/united_states/nba/"
+season = "2007-2008"
+files = os.listdir(directory + season)
+
+season_avg_teams = defaultdict(list)
+season_avg_players = defaultdict(list)
+last_ten_avg_teams = defaultdict(list)
+last_ten_teams = defaultdict(list)
+
+team_game_index = {}
+
+season_data = pd.DataFrame() ### need to specify columns
+
+for file in files:
+	if ".json" not in file: continue
+
+	with open(file) as json_data:
+		    game_data = json.load(json_data)
+		    json_data.close()
+
+	
+	#################### GAME TABLE
+	## initialize the data entries
+	stats = {}
+
+	## location ??
+	#stats["Location"] = 
+
+	## index: team1-team2
+	stats["index"] = game_data["home"]["name"]+game_data["away"]["name"]
+
+	## date
+	stats["date"] = game_data["code"][:-3]
+
+	## team names
+	stats["home_name"] = game_data["home"]["name"]
+	stats["away_name"] = game_data["away"]["name"]
+
+	for name, value in game_data["home"]["totals"].iteritems():
+		stats["home_" + name] = value
+
+	for name, value in game_data["away"]["totals"].iteritems():
+		stats["away_" + name] = value
+
+
+
+
+
+
+print(game_data["code"])
+print(game_data["away"]["name"])
+#print(game_data["away"]["totals"])
+print(game_data["away"]["scores"])
+print(game_data["home"]["name"])
+#print(game_data["home"]["totals"])
+print(game_data["home"]["scores"])
+
