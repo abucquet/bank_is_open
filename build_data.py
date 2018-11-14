fdf28ade16c7ca7a6293b7e40d295ee153d1cb5b
VSAnimator <sarukkai@stanford.edu>
2018-11-13 19:22:46 -0600

    Partial data processing

--- build_data.py
+++ build_data.py
@@ -23,8 +23,9 @@ def compile_season(season, directory="basketball_reference-master/matches/united
 	team_game_index = defaultdict(int)
 
 	season_data = pd.DataFrame() ### need to specify columns
+	player_date_data = pd.DataFrame()
 
-	for file in files:
+	for file in files[1:10]:
 		if ".json" not in file: continue
 
 		with open(directory + season + "/" + file) as json_data:
@@ -48,10 +49,10 @@ def compile_season(season, directory="basketball_reference-master/matches/united
 		stats["home_name"] = game_data["home"]["name"]
 		stats["away_name"] = game_data["away"]["name"]
 
-		for name, value in game_data["home"]["totals"].iteritems():
+		for name, value in game_data["home"]["totals"].items():
 			stats["home_" + name] = value
 
-		for name, value in game_data["away"]["totals"].iteritems():
+		for name, value in game_data["away"]["totals"].items():
 			stats["away_" + name] = value
 
 		## adding the stats to the df
@@ -68,21 +69,35 @@ def compile_season(season, directory="basketball_reference-master/matches/united
 	# print(team_game_index)
 
 		###################### PLAYER TABLE
+		players = defaultdict(list)
 		## One row per player per game
-		players = {}
-		for team in game_data:	# "home" and "away". Should loop over keys
-			for entry in game_data[team]:
+		for team in ["home","away"]:	# "home" and "away". Should loop over keys
+			for entry in game_data[team]["players"]:
 				# Get team for player (in case they switch teams)
 				# Loop over players and fill table
+				players["name"].append(entry)
+				players["date"].append(game_data["code"][:-3])
+				players["team"].append(game_data[team]["name"])
+				for name, value in game_data[team]["players"][entry].items():
+					players[name].append(value)
+
+				#player_date_data = player_date_data.append(player, ignore_index=True)
+
+		#player_date_data = pd.DataFrame(columns=players.keys())
+		#player_date_data = player_date_data.append(players, ignore_index=True)
+		player_date_data = pd.DataFrame(players)
+
 
 	## SOME SORT OF ERROR CHECKING TO MAKE SURE WE HAVE ALL THE DATA FOR A SEASON:
 	# Make sure we have all the regular-season games
 	# Make sure that player data is complete
 	# Simple sanity check on the numbers making sense
 
-	return season_data
+	return season_data, player_date_data
 
 
 if __name__ == "__main__":
-	df = compile_season("2007-2008")
+	df, player_df = compile_season("2007-2008")
 	print(df.shape)
+	df.to_csv("./testcsv.csv")
+	player_df.to_csv("./testcsv2.csv")
